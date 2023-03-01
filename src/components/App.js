7fd85365bec6600b6e077093b137ad9406d11c64
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-03-01 14:07:53 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import React from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <ListingsContainer />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+            <ListingsContainer />
+        </div>
+    );
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Search from "./Search";
 
-function Header() {
-  return (
-    <header>
-      <h1>
-        <span className="logo" role="img">
-          ☮
-        </span>
-        gregslist
-      </h1>
-      <Search />
-    </header>
-  );
-}
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <span className="logo" role="img">
+                    ☮
+                </span>
+                gregslist
+            </h1>
+            <Search />
+        </header>
+    );
+};
 
 export default Header;
